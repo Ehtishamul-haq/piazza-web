a1455fcc51ed3da3ab436de1c0f20c31e685a24f
Ehtishamul Haq <ehtishamqasmi@gmail.com>
2023-11-02 16:25:23 +0530

    Lay out the navigation bar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = [ "burger", "menu" ]
+  toggle() {
+    this.burgerTarget.classList.toggle("is-active")
+    this.menuTarget.classList.toggle("is-active")
+  }
+}
+
+
+// export default class extends Controller {
+//   static targets = [ "burger", "menu" ]
+//   toggle() {
+//     this.burgerTarget.classList.toggle("is-active")
+//     this.menuTarget.classList.toggle("is-active")
+//   }
+// }
